cf1a90b9575848ea20b2c31c9bcab28fd6bf5866
MUROMACHI Toshiki <37264933+ToshikiMuromachi@users.noreply.github.com>
2020-07-26 02:29:24 +0900

    Update trainer.py

--- src/models/trainer.py
+++ src/models/trainer.py
@@ -293,9 +293,12 @@ class Trainer(object):
                             save_gold.write(gold[i].strip()+'\n')
                         for i in range(len(pred)):
                             save_pred.write(pred[i].strip()+'\n')
+        # 20200726
+        '''
         if(step!=-1 and self.args.report_rouge):
             rouges = test_rouge(self.args.temp_dir, can_path, gold_path)
             logger.info('Rouges at step %d \n%s' % (step, rouge_results_to_str(rouges)))
+        '''
         self._report_step(0, step, valid_stats=stats)
 
         return stats
